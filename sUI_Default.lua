9a2e6f7f154a7b5f7b023602dbeff860ec6c5087
Dalaran.city <x@dalaran.city>
2017-12-26 09:38:17 +0800

    chore: init repo

---
+++ sUI_Default.lua
@@ -0,0 +1,47 @@
+-- This file is loaded from "sUI Default.toc"
+-- http://wow.gamepedia.com/Category:Console_variables
+
+-- General
+SetCVar("alwaysCompareItems", 0)
+SetCVar("autoLootDefault", 1)
+SetCVar("lootUnderMouse", 1)
+
+-- Unit frames
+SetCVar("showTargetOfTarget", 1)
+
+-- Combat related
+SetCVar("enableFloatingCombatText", 1)
+
+-- Nameplates
+SetCVar("nameplateMaxDistance", 40)
+SetCVar("nameplateOtherTopInset", -1)
+SetCVar("nameplateOtherBottomInset", -1)
+
+-- Action bars
+SetCVar("alwaysShowActionBars", 1)
+SetCVar("lockActionBars", 1)
+SetCVar("countdownForCooldowns", 1)
+
+-- Raid / Party
+SetCVar("raidFramesDisplayClassColor", 1)
+SetCVar("raidFramesDisplayOnlyDispellableDebuffs", 1)
+SetCVar("raidOptionKeepGroupsTogether", 1)
+SetCVar("useCompactPartyFrames", 1)
+
+-- Chat related
+SetCVar("chatBubbles", 1)
+SetCVar("chatBubblesParty", 0)
+SetCVar("colorChatNamesByClass", 1)
+SetCVar("guildMemberNotify", 0)
+SetCVar("whisperMode", "inline")
+
+-- Chat filter
+SetCVar("profanityFilter", 0)
+SetCVar("spamFilter", 1)
+
+-- Camera view
+SetCVar("cameraSmoothStyle", 0)
+
+-- Miscs
+SetCVar("overrideArchive", 0)
+SetCVar("screenshotQuality", 10)
